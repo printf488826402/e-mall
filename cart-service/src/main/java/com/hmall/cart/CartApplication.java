26606ea289a655379368b2c17c1adeec3b2e3065
printf488826402 <17352720986@163.com>
2025-08-23 21:47:06 +0800

    无

--- cart-service/src/main/java/com/hmall/cart/CartApplication.java
+++ cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -1,12 +1,14 @@
 package com.hmall.cart;
 
+import com.hmall.api.client.config.DefaultFeignConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-@EnableFeignClients(basePackages = {"com.hmall.api.client"})//能用import替代吗？
+@EnableFeignClients(basePackages = "com.hmall.api.client", defaultConfiguration = DefaultFeignConfig.class)//能用import替代吗？
+//FeignClient全局配置日志
 @MapperScan("com.hmall.cart.mapper")
 @SpringBootApplication
 public class CartApplication {
